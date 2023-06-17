39b18f1a72da4bb6286c58b40b202752d55ea3b0
Kamilya <you@example.com>
2023-06-17 14:22:35 +0300

    add Hello Tom

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("What is your name:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tom")
+{
+    Console.WriteLine("Have a good day, Tom!");
+}
+else
+{
+    Console.Write("Hello,  ");
+    Console.WriteLine(username);
+}
